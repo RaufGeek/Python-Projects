b09d8b10a21504d4e1649a3fc6b305cd64eac79c
RaufGeek <125791030+RaufGeek@users.noreply.github.com>
2023-02-20 23:27:45 +0400

    Add files via upload

---
+++ QRCodeGenerator.py
@@ -0,0 +1,20 @@
+import qrcode
+
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("raufgeek.png")
+
+
+generate_qrcode("https://github.com/RaufGeek/Python-Projects")
+
