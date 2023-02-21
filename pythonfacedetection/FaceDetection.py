85306431c236c56f7b3246c40b0682c16430ff0e
Rauf <125791030+RaufGeek@users.noreply.github.com>
2023-02-21 12:48:57 +0400

    Update FaceDetection.py

--- pythonfacedetection/FaceDetection.py
+++ pythonfacedetection/FaceDetection.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('classphoto.jpg')
+img = cv2.imread('#YOURPHOTO.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
